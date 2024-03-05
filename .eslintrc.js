d5bf0a176bd6ff5ba2455464896a41024d7482e0
charles <charles_76@163.com>
2024-03-05 22:41:10 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ["react-app"],
+  env: {
+    node : true,
+    browser: true, // 允许使用浏览器全局变量
+  },
   parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
     babelOptions: {
       presets: [
         /* -------------------------------- 解决页面报错问题 -------------------------------- */
@@ -8,5 +14,8 @@ module.exports = {
         'babel-preset-react-app/prod',
       ]
     }
+  },
+  rules: {
+    "no-var": 2,
   }
 }
