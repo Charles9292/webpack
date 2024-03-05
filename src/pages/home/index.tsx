6dc638d14ee3120f101cc06f775fdbbd4be12ad2
charles <charles_76@163.com>
2024-03-05 22:41:44 +0800

    feat: 源码

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,9 @@ import './index.less'
 const baseClass = 'home'
 
 export default function index() {
+  const sum = () => {
+    console.log("%c Line:9 🥤", "color:#3f7cff");
+  }
   return (
     <div className={baseClass}>home</div>
   )
