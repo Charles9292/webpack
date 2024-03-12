2624065c7fd1209f36d4747b4e98888a1de0ab46
charles <charles_76@163.com>
2024-03-12 23:27:21 +0800

    feat: 动态导入

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
-import Home from './pages/home'
+// import Home from './pages/home'
 
 function App() {
   return (
     <>
       <div>App1</div>
-      <Home />
+      {
+        import('./pages/home').then(module => {
+          const Home = module.default
+          return <Home />
+        })
+      }
+      
+      {/* <Home /> */}
     </>
   )
 }
